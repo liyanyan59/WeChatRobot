138a0433a5b76d5b45d75a1b3678d775164df2f2
ly <liyan6987@gmail.com>
2019-03-25 20:21:59 +0800

    第一次上传有点激动！！

---
+++ robot.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/3/25 19:10
+# @Author  : LY
+# @FileName: robot
+# @Software: PyCharm
+# @Official Accounts：大数据学习废话集
+import ast
+import re
+
+import werobot
+import requests
+import json
+
+robot = werobot.WeRoBot(token='tokenhere')
+
+
+@robot.text
+def text_(message):
+    return text_process(message)
+
+
+# @robot.image
+# def image_(message):
+#     return image_process()
+
+
+post_url = 'https://www.aixxz.com/test/ApiTest_main.php?randkey=R9UThn77gX5YMhugP32C'
+
+
+def text_process(message):
+    text = message.content
+    res = requests.post(post_url, data={'text': text, 'aixxz_api': '/api3'})
+    reply = res.content.decode('utf-8', 'ignore')
+    reply = re.findall('返回:jason.*?\r\n(.*)', reply)[0].strip().replace('\\', '')
+    reply = json.loads(reply)
+    data = reply['data']
+    return data
+
+
+# 让服务器监听在 0.0.0.0:6987
+robot.config['HOST'] = '0.0.0.0'
+robot.config['PORT'] = 6987
+robot.run()
